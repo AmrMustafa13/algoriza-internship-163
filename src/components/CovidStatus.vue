e2801136d8717067f8e61051d9e1fc265b5c797b
AmrMustafa13 <amrmostafa1311@gmail.com>
2023-11-24 17:27:08 +0200

    add covid component

---
+++ src/components/CovidStatus.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="bg-[#FCEFCA] text-[#333] flex items-center gap-5 p-6 rounded-md my-16"
+  >
+    <img src="../assets/images/danger.png" alt="danger" />
+    <p>
+      Check the latest COVID-19 restrictions before you travel.
+      <span class="text-[#2F80ED] cursor-pointer">Learn more</span>
+    </p>
+  </div>
+</template>
--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -2,10 +2,12 @@
   <div class="container">
     <Navbar />
     <Header />
+    <CovidStatus />
   </div>
 </template>
 
 <script setup>
 import Navbar from "../../components/Navbar.vue";
 import Header from "./components/Header.vue";
+import CovidStatus from "../../components/CovidStatus.vue";
 </script>
