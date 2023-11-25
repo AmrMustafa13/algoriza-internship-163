8831126c41855dcbe8118256f5ab084bae3d2eca
AmrMustafa13 <amrmostafa1311@gmail.com>
2023-11-25 14:56:41 +0200

    add dream vacation section

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -3,6 +3,7 @@
     <Navbar />
     <Header />
     <CovidStatus />
+    <DreamVacation />
   </div>
 </template>
 
@@ -10,4 +11,5 @@
 import Navbar from "../../components/Navbar.vue";
 import Header from "./components/Header.vue";
 import CovidStatus from "../../components/CovidStatus.vue";
+import DreamVacation from "./components/DreamVacation.vue";
 </script>
---
+++ src/views/Home/components/DreamVacation.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="text-[#181818] flex flex-col gap-4">
+    <h2 class="font-semibold text-2xl">Enjoy your dream vacation</h2>
+    <p class="text-[#333]">
+      Plan and book our perfect trip with expert advice, travel tips,
+      destination information and inspiration from us
+    </p>
+    <ul class="py-8 grid grid-cols-4 list-none gap-5">
+      <li class="flex flex-col gap-4">
+        <img
+          src="../../../assets/images/dream-vacation/Australia.jpeg"
+          alt="Australia"
+          class="rounded-lg h-56 object-cover"
+        />
+        <div>
+          <h3>Australia</h3>
+          <span class="text-sm">2246 properties</span>
+        </div>
+      </li>
+      <li class="flex flex-col gap-4">
+        <img
+          src="../../../assets/images/dream-vacation/Japan.jpeg"
+          alt="Japan"
+          class="rounded-lg h-56 object-cover"
+        />
+        <div>
+          <h3>Australia</h3>
+          <span class="text-sm">1278 properties</span>
+        </div>
+      </li>
+      <li class="flex flex-col gap-4">
+        <img
+          src="../../../assets/images/dream-vacation/New-Zealand.jpeg"
+          alt="New Zealand"
+          class="rounded-lg h-56 object-cover"
+        />
+        <div>
+          <h3>New Zealand</h3>
+          <span class="text-sm">480 properties</span>
+        </div>
+      </li>
+      <li class="flex flex-col gap-4">
+        <img
+          src="../../../assets/images/dream-vacation/Greece.jpeg"
+          alt="Greece"
+          class="rounded-lg h-56 object-cover"
+        />
+        <div>
+          <h3>Greece</h3>
+          <span class="text-sm">320 properties</span>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
