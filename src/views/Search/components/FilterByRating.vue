040ca738efb6f0230e129c98961f085f5efb418a
AmrMustafa13 <amrmostafa1311@gmail.com>
2023-11-28 18:02:02 +0200

    add filter by rating

--- src/views/Search/components/FilterByRating.vue
+++ src/views/Search/components/FilterByRating.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>rating</div>
+  <div class="rounded-md flex flex-col gap-2 border border-[#E2E2E2]">
+    <h3 class="text-[#181818] text-[500] bg-[#F2F2F2] p-4">Rating</h3>
+    <p class="text-[#4f4f4f] text-sm p-4">Show only ratings more than</p>
+    <div
+      class="flex justify-between items-center rounded-md mb-4 mx-4 bg-white border border-[#E2E2E2]"
+    >
+      <div
+        class="flex justify-center items-center gap-1 cursor-pointer border-r border-[#e2e2e2] p-4"
+      >
+        <span>1</span>
+        <img src="../../../assets/images/stars/full.svg" alt="full-star" />
+      </div>
+      <div
+        class="flex justify-center items-center gap-1 cursor-pointer border-r border-[#e2e2e2] p-4"
+      >
+        <span>2</span>
+        <img src="../../../assets/images/stars/full.svg" alt="full-star" />
+      </div>
+      <div
+        class="flex justify-center items-center gap-1 cursor-pointer border-r border-[#e2e2e2] p-4"
+      >
+        <span>3</span>
+        <img src="../../../assets/images/stars/full.svg" alt="full-star" />
+      </div>
+      <div
+        class="flex justify-center items-center gap-1 cursor-pointer border-r border-[#e2e2e2] p-4"
+      >
+        <span>4</span>
+        <img src="../../../assets/images/stars/full.svg" alt="full-star" />
+      </div>
+      <div class="flex justify-center items-center gap-1 cursor-pointer p-4">
+        <span>5</span>
+        <img src="../../../assets/images/stars/full.svg" alt="full-star" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
