0aa54224a61815b4f471b23557df520308c0eab3
AmrMustafa13 <amrmostafa1311@gmail.com>
2023-11-28 20:05:10 +0200

    add hotel gallery

--- src/views/HotelDetails/HotelDetails.vue
+++ src/views/HotelDetails/HotelDetails.vue
@@ -1,5 +1,66 @@
 <template>
-  <div>Hotel details</div>
+  <Navbar />
+  <div v-if="isLoading">
+    <LoadingSpinner />
+  </div>
+  <div class="bg-[#f4f4f4]" v-else>
+    <div class="bg-gradient-to-b from-[#F4F4F400] to-[#fff]">
+      <div class="container">
+        <HotelGallery />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRoute } from "vue-router";
+import axios from "axios";
+import { useHotelsStore } from "../../stores/hotels";
+import { onMounted, ref } from "vue";
+import LoadingSpinner from "../../components/LoadingSpinner.vue";
+import Navbar from "../../components/Navbar.vue";
+import HotelGallery from "./components/HotelGallery.vue";
+
+const hotelsStore = useHotelsStore();
+
+const searchQueries = hotelsStore.searchQueries;
+
+const route = useRoute();
+
+const hotelId = route.params.id;
+
+const hotelDetails = ref({});
+const isLoading = ref(false);
+
+const fetchHotelDetails = async () => {
+  const options = {
+    method: "GET",
+    url: "https://booking-com15.p.rapidapi.com/api/v1/hotels/getHotelDetails",
+    params: {
+      hotel_id: hotelId,
+      arrival_date: searchQueries.checkIn,
+      departure_date: searchQueries.checkOut,
+      adults: searchQueries.guests,
+      room_qty: searchQueries.rooms,
+    },
+    headers: {
+      "X-RapidAPI-Key": import.meta.env.VITE_RAPIDAPI_KEY,
+      "X-RapidAPI-Host": "booking-com15.p.rapidapi.com",
+    },
+  };
+
+  try {
+    isLoading.value = true;
+    const response = await axios.request(options);
+    console.log(response.data.data);
+    hotelDetails.value = response.data.data;
+    isLoading.value = false;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+onMounted(() => {
+  fetchHotelDetails();
+});
+</script>
---
+++ src/views/HotelDetails/components/HotelGallery.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex justify-between gap-5 py-5">
+    <div class="flex-[2]">
+      <img
+        src="../../../assets/images/hotel-gallery/1.webp"
+        alt="main"
+        class="rounded-md h-[450px] w-full object-cover"
+      />
+    </div>
+    <div class="flex-1 flex flex-col gap-5">
+      <div>
+        <img
+          src="../../../assets/images/hotel-gallery/2.webp"
+          alt="sec"
+          class="rounded-md h-[215px] w-full object-cover"
+        />
+      </div>
+      <div>
+        <img
+          src="../../../assets/images/hotel-gallery/3.webp"
+          alt="third"
+          class="rounded-md h-[217px] w-full object-cover"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
