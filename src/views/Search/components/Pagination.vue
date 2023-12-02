04be0f57fde7dfa200b51c4528fa3a3a6c28bfa3
AmrMustafa13 <amrmostafa1311@gmail.com>
2023-12-02 17:52:09 +0200

    fix pagination types

--- src/views/Search/components/Pagination.vue
+++ src/views/Search/components/Pagination.vue
@@ -3,14 +3,14 @@
     <ul class="flex">
       <li
         :class="{
-          'text-[#2F80ED]': currentPage !== 1,
-          'text-[#9e9e9e]': currentPage === 1,
+          'text-[#2F80ED]': +currentPage !== 1,
+          'text-[#9e9e9e]': +currentPage === 1,
         }"
         class="font-[500] rounded-lg w-10 h-10 p-2 flex justify-center items-center shadow-lg mr-2"
       >
         <button
-          :disabled="currentPage === 1"
-          @click="hotelsStore.changePage(currentPage - 1)"
+          :disabled="+currentPage === 1"
+          @click="hotelsStore.changePage(+currentPage - 1)"
         >
           {{ `<` }}
         </button>
@@ -18,37 +18,37 @@
       <li>
         <button
           :class="{
-            'bg-[#2F80ED] text-white': currentPage === currentPage,
-            'bg-[#fff] text-[#9e9e9e]': currentPage !== currentPage,
+            'bg-[#2F80ED] text-white': +currentPage === +currentPage,
+            'bg-[#fff] text-[#9e9e9e]': +currentPage !== +currentPage,
           }"
           class="font-[500] rounded-lg w-10 h-10 p-2"
-          @click="hotelsStore.changePage(currentPage)"
+          @click="hotelsStore.changePage(+currentPage)"
         >
-          {{ currentPage }}
+          {{ +currentPage }}
         </button>
       </li>
       <li
-        v-if="currentPage + 1 <= totalPages"
+        v-if="+currentPage + 1 <= totalPages"
         :class="{
-          'bg-[#2F80ED] text-white': currentPage + 1 === currentPage,
-          'bg-[#fff] text-[#9e9e9e]': currentPage + 1 !== currentPage,
+          'bg-[#2F80ED] text-white': +currentPage + 1 === currentPage,
+          'bg-[#fff] text-[#9e9e9e]': +currentPage + 1 !== +currentPage,
         }"
         class="font-[500] rounded-lg w-10 h-10 p-2"
       >
-        <button @click="hotelsStore.changePage(currentPage + 1)">
-          {{ currentPage + 1 }}
+        <button @click="hotelsStore.changePage(+currentPage + 1)">
+          {{ +currentPage + 1 }}
         </button>
       </li>
       <li
         v-if="currentPage + 2 <= totalPages"
         :class="{
-          'bg-[#2F80ED] text-white': currentPage + 2 === currentPage,
-          'bg-[#fff] text-[#9e9e9e]': currentPage + 2 !== currentPage,
+          'bg-[#2F80ED] text-white': +currentPage + 2 === +currentPage,
+          'bg-[#fff] text-[#9e9e9e]': +currentPage + 2 !== +currentPage,
         }"
         class="font-[500] rounded-lg w-10 h-10 p-2"
       >
-        <button @click="hotelsStore.changePage(currentPage + 2)">
-          {{ currentPage + 2 }}
+        <button @click="hotelsStore.changePage(+currentPage + 2)">
+          {{ +currentPage + 2 }}
         </button>
       </li>
       <li class="font-[500] rounded-lg w-10 h-10 p-2 text-[#9e9e9e]">
@@ -56,13 +56,13 @@
       </li>
       <li
         :class="{
-          'bg-[#2F80ED] text-white': currentPage === totalPages,
-          'bg-[#fff] text-[#9e9e9e]': currentPage !== totalPages,
+          'bg-[#2F80ED] text-white': +currentPage === totalPages,
+          'bg-[#fff] text-[#9e9e9e]': +currentPage !== totalPages,
         }"
         class="font-[500] rounded-lg w-10 h-10 p-2"
       >
         <button
-          :disabled="currentPage === totalPages"
+          :disabled="+currentPage === totalPages"
           @click="hotelsStore.changePage(totalPages)"
         >
           {{ totalPages }}
@@ -70,14 +70,14 @@
       </li>
       <li
         :class="{
-          'text-[#2F80ED]': currentPage !== totalPages,
-          'text-[#9e9e9e]': currentPage === totalPages,
+          'text-[#2F80ED]': +currentPage !== totalPages,
+          'text-[#9e9e9e]': +currentPage === totalPages,
         }"
         class="font-[500] rounded-lg w-10 h-10 p-2 flex items-center justify-center shadow-lg ml-2"
       >
         <button
-          :disabled="currentPage === totalPages"
-          @click="hotelsStore.changePage(currentPage + 1)"
+          :disabled="+currentPage === totalPages"
+          @click="hotelsStore.changePage(+currentPage + 1)"
           class="flex items-center justify-center"
         >
           {{ `>` }}
